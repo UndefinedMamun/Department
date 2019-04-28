d7edf009d0dd75ba9744798a2665aebb1aabb95c
undefinedmamun <bkmmamun@gmail.com>
2019-04-28 07:35:01 +0600

    post deployment adujustment

--- routes/avgDuration.js
+++ routes/avgDuration.js
@@ -3,32 +3,52 @@ var router = express.Router();
 const Titles = require("../models/titles");
 
 /* GET Oldest Employee page. */
-router.get("/", function (req, res, next) {
-  Titles.aggregate([
-    { $match: {} },
-    { $project: { time: { $subtract: [{ $dateFromString: { dateString: "$to_date" } }, { $dateFromString: { dateString: "$from_date" } }] } } },
-    {
-      $group: {
-        _id: null,
-        durationAvg: { $avg: "$time" }
+router.get("/", function(req, res, next) {
+  Titles.aggregate(
+    [
+      // { $match: {} },
+      {
+        $project: {
+          title: "$title",
+          time: {
+            $subtract: [
+              { $dateFromString: { dateString: "$to_date" } },
+              { $dateFromString: { dateString: "$from_date" } }
+            ]
+          }
+        }
+      },
+      {
+        $group: {
+          _id: "$title",
+          durationAvg: { $avg: "$time" }
+        }
+      },
+      {
+        $sort: { durationAvg: -1 }
       }
-    }
-  ], (err, result) => {
-    if (err) {
-      console.log(err.message);
-      return;
-    }
-    // console.log(result)
-    const date = new Date(result[0].durationAvg);
-    const time = {
-      year: date.getFullYear(),
-      month: date.getMonth() + 1,
-      day: date.getDay()
-    }
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err.message);
+        return;
+      }
+      // console.log(result);
+      const formatedTime = result.map(single => {
+        const date = new Date(single.durationAvg);
+        const time = {
+          year: date.getFullYear(),
+          month: date.getMonth() + 1,
+          day: date.getDay()
+        };
+        single.time = time;
+        return single;
+      });
 
-    // console.log(time);
-    res.render("avgDuration", { time });
-  });
+      // console.log(formatedTime);
+      res.render("avgDuration", { formatedTime });
+    }
+  );
 });
 
 module.exports = router;
